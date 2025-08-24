95d6388e4d057fe33a904da103ca28a02495f970
Oom Lout <aaron@oomlout.com>
2025-08-24 19:15:10 +0100

    auto commit

--- scad.py
+++ scad.py
@@ -20,18 +20,20 @@ def make_scad(**kwargs):
         typ = "fast"
         #typ = "manual"
 
-    oomp_mode = "project"
-    #oomp_mode = "oobb"
+    #oomp_mode = "project"
+    oomp_mode = "oobb"
 
     test = False
     #test = True
 
     if typ == "all":
-        filter = ""; save_type = "all"; navigation = True; overwrite = True; modes = ["3dpr"]; oomp_run = True; test = False
+        #no overwrite
+        filter = ""; save_type = "all"; navigation = True; overwrite = False; modes = ["3dpr","laser"]; oomp_run = True; test = False
         #default
         #filter = ""; save_type = "all"; navigation = True; overwrite = True; modes = ["3dpr"]; oomp_run = True; test = False
     elif typ == "fast":
-        filter = ""; save_type = "none"; navigation = False; overwrite = True; modes = ["3dpr"]; oomp_run = False
+        #navigation true
+        filter = ""; save_type = "none"; navigation = True; overwrite = True; modes = ["3dpr"]; oomp_run = False
         #default
         #filter = ""; save_type = "none"; navigation = False; overwrite = True; modes = ["3dpr"]; oomp_run = False
     elif typ == "manual":
@@ -112,43 +114,179 @@ def make_scad(**kwargs):
             kwargs["oomp_manufacturer"] = ""
             kwargs["oomp_part_number"] = ""
 
-        part_default = {} 
-       
-        part_default["project_name"] = project_name
-        part_default["full_shift"] = [0, 0, 0]
-        part_default["full_rotations"] = [0, 0, 0]
         
-        part = copy.deepcopy(part_default)
-        p3 = copy.deepcopy(kwargs)
-        p3["width"] = 3
-        p3["height"] = 3
-        #p3["thickness"] = 6
-        #p3["extra"] = ""
-        part["kwargs"] = p3
-        nam = "base"
-        part["name"] = nam
-        if oomp_mode == "oobb":
-            p3["oomp_size"] = nam
-        if not test:
-            pass
-            #parts.append(part)
+        #defining all the plates
+        if True:
+            plates = []
+    
+    
+            #sizes = ["oobb", "oobe"]
+            sizes = ["oobb"]
+            
+            #base plates
+            
+            #test = True
+            test = False
+            
+            if not test:
+                for size in sizes:
+                    #all 3m thicks 1x1 to 10x10
+                    for wid in range(3, 11):
+                        for hei in range(3, 11):
+                            if wid >= hei:
+                                plates.append({"type": "plate_u", "width": wid,
+                                            "height": hei, "thickness": 3, "size": size})
+            elif test:
+                plates.append({"type": "plate_u", "width": 5,
+                                            "height": 5, "thickness": 3, "size": "oobb"})
+            
+                
+            
+        
+        
+        #defining them as parts
+        if True:
 
+            extras = []            
+            extras.append("")
+            
+            
 
-    kwargs["parts"] = parts
+            part_default = {} 
+        
+            part_default["project_name"] = project_name
+            part_default["full_shift"] = [0, 0, 0]
+            part_default["full_rotations"] = [0, 0, 0]
+            
+
+            for ex in extras:
+                for plate in plates:
+                    extra = plate.get("extra", "")
+                    wid = plate["width"]
+                    hei = plate["height"]
+                    dep = plate["thickness"]
+                    typ = plate.get("type", "plate")
+                    if True:
+                        part = copy.deepcopy(part_default)
+                        p3 = copy.deepcopy(kwargs)
+                        p3["width"] = wid
+                        p3["height"] = hei
+                        p3["thickness"] = dep
+                        if ex != "":
+                            p3["extra"] = ex
+                        part["kwargs"] = p3
+                        part["name"] = typ
+                        if oomp_mode == "oobb":
+                            p3["oomp_size"] = typ
+                        if True:
+                            pass
+                            parts.append(part)
+                   
+
+        kwargs["parts"] = parts
 
     scad_help.make_parts(**kwargs)
 
     #generate navigation
     if navigation:
-        sort = []
-        #sort.append("extra")
+        sort = []        
         sort.append("name")
         sort.append("width")
         sort.append("height")
         sort.append("thickness")
+        sort.append("extra")
         
         scad_help.generate_navigation(sort = sort)
 
+
+def get_plate_u(thing, **kwargs):
+
+    prepare_print = kwargs.get("prepare_print", False)
+    width = kwargs.get("width", 1)
+    height = kwargs.get("height", 1)
+    depth = kwargs.get("thickness", 3)                    
+    rot = kwargs.get("rot", [0, 0, 0])
+    pos = kwargs.get("pos", [0, 0, 0])
+    extra = kwargs.get("extra", "")
+    
+    #add plate ups
+    p3 = copy.deepcopy(kwargs)
+    p3["type"] = "positive"
+    p3["shape"] = f"oobb_plate"    
+    p3["width"] = 1
+    p3["depth"] = depth
+    #p3["holes"] = True         uncomment to include default holes
+    #p3["m"] = "#"
+    pos1 = copy.deepcopy(pos)         
+    poss = []
+    pos11 = copy.deepcopy(pos1)
+    pos11[0] += (width-1) / 2 * 15
+    poss.append(pos11)
+    pos12 = copy.deepcopy(pos1)
+    pos12[0] += -(width-1) / 2 * 15
+    poss.append(pos12)
+
+    p3["pos"] = poss
+    oobb_base.append_full(thing,**p3)
+    
+    #add plate bottom
+    p3 = copy.deepcopy(kwargs)
+    p3["type"] = "positive"
+    p3["shape"] = f"oobb_plate"
+    p3["height"] = 1
+    p3["depth"] = depth
+    #p3["holes"] = True         uncomment to include default holes
+    #p3["m"] = "#"
+    pos1 = copy.deepcopy(pos)
+    poss = []
+    pos11 = copy.deepcopy(pos1)
+    pos11[1] += -(height-1) / 2 * 15
+    poss.append(pos11)
+    p3["pos"] = poss
+    oobb_base.append_full(thing,**p3)
+
+
+    #add holes seperate
+    p3 = copy.deepcopy(kwargs)
+    p3["type"] = "p"
+    p3["shape"] = f"oobb_holes"
+    if extra == "":
+        p3["both_holes"] = True  
+        p3["holes"] = ["top", "bottom", "left"]
+    p3["depth"] = depth
+    p3["m"] = "#"
+    pos1 = copy.deepcopy(pos)         
+    p3["pos"] = pos1
+    oobb_base.append_full(thing,**p3)
+
+    
+
+    if prepare_print:
+        #put into a rotation object
+        components_second = copy.deepcopy(thing["components"])
+        return_value_2 = {}
+        return_value_2["type"]  = "rotation"
+        return_value_2["typetype"]  = "p"
+        pos1 = copy.deepcopy(pos)
+        pos1[0] += 50
+        return_value_2["pos"] = pos1
+        return_value_2["rot"] = [180,0,0]
+        return_value_2["objects"] = components_second
+        
+        thing["components"].append(return_value_2)
+
+    
+        #add slice # top
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "n"
+        p3["shape"] = f"oobb_slice"
+        pos1 = copy.deepcopy(pos)
+        pos1[0] += -500/2
+        pos1[1] += 0
+        pos1[2] += -500/2        
+        p3["pos"] = pos1
+        #p3["m"] = "#"
+        oobb_base.append_full(thing,**p3)
 
 def get_base(thing, **kwargs):
 
@@ -209,7 +347,8 @@ def get_base(thing, **kwargs):
         p3["pos"] = pos1
         #p3["m"] = "#"
         oobb_base.append_full(thing,**p3)
-    
+
+
 if __name__ == '__main__':
     kwargs = {}
     main(**kwargs)
---
+++ scad_output/readme.md
@@ -0,0 +1,9 @@
+# Oomlout oobb part plate u
+
+
+## parts
+
+### 5_width_5_height_3_mm_depth
+* link: [/5_width_5_height_3_mm_depth](5_width_5_height_3_mm_depth)  
+![](5_width_5_height_3_mm_depth/3dpr_300.png)  ![](5_width_5_height_3_mm_depth/image_300.jpg)
+ 
